01f1c4a118046a4c5914dd6ea0eb05a3758046a6
prakashkrish2022 <prakashmech925@gmail.com>
2022-04-22 09:48:17 -0700

    new code added

---
+++ src/org/all/AllLaunage.java
@@ -0,0 +1,18 @@
+package org.all;
+
+public class AllLaunage {
+
+	public void tamil() {
+		System.out.println(" tamil mark is 98");
+	}
+private void English() {
+System.out.println(" english mark is 90");
+}
+	public static void main(String[] args) {
+AllLaunage l=new AllLaunage();
+l.English();
+	l.tamil();
+	
+	}
+
+}
